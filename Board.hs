5121879ffb0ac895884634e6cfcd29d81f1c1eaf
akzfowl <akshay10791@gmail.com>
2018-11-21 20:13:51 -0500

    Small correction and added functions to convert positions

--- Board.hs
+++ Board.hs
@@ -7,7 +7,29 @@ import Piece
 type Square = Maybe Piece
 type Board = [[Square]]
 type BoardPosition = (Int, Int)
-type HumanReadabalePosition = (Char, Int)
+type HumanReadablePosition = (Char, Int)
+
+boardToHumanReadable :: BoardPosition -> HumanReadablePosition
+boardToHumanReadable (p1, p2) = case p1 of
+    1 -> ('a',p2)
+    2 -> ('b',p2)
+    3 -> ('c',p2)
+    4 -> ('d',p2)
+    5 -> ('e',p2)
+    6 -> ('f',p2)
+    7 -> ('g',p2)
+    8 -> ('h',p2)
+
+humanReadableToBoard :: HumanReadablePosition -> BoardPosition
+humanReadableToBoard (p1, p2) = case p1 of
+    'a' -> (1,p2)
+    'b' -> (2,p2)
+    'c' -> (3,p2)
+    'd' -> (4,p2)
+    'e' -> (5,p2)
+    'f' -> (6,p2)
+    'g' -> (7,p2)
+    'h' -> (8,p2)
 
 initialBoard :: Board
 initialBoard = [[Just(Piece White Rook), Just(Piece White Knight), Just(Piece White Bishop), Just(Piece White Queen), Just(Piece White King), Just(Piece White Bishop), Just(Piece White Knight), Just(Piece White Rook)],
